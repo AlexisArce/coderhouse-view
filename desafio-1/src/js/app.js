62eac911003ecdd456320064fb66c14cf66b08aa
aarce <Arce.Alexis@hotmail.com>
2021-11-15 21:29:56 -0300

    Add two additional info outputs

--- desafio-1/src/js/app.js
+++ desafio-1/src/js/app.js
@@ -2,14 +2,24 @@ let app = new Vue({
   el: "#app",
   data: {
     result: 0,
+    clicksCounter: 0,
   },
   methods: {
     add() {
+      this.clicksCounter++;
       return this.result++;
     },
     subtract() {
+      this.clicksCounter++;
       return this.result--;
     },
   },
-  computed: {},
+  computed: {
+    clicks() {
+      return this.clicksCounter;
+    },
+    squared() {
+      return this.clicksCounter * this.clicksCounter;
+    },
+  },
 });
