eb5855ab6f32a301258943a6034486058ab3c642
aarce <Arce.Alexis@hotmail.com>
2021-12-06 00:41:56 -0300

    Add home component

--- proyecto-final/src/App.vue
+++ proyecto-final/src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <v-row>
-          <v-col md="6" offset-md="3">
-            <login />
-          </v-col>
-        </v-row>
-      </v-container>
+      <home />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Login from "./components/login/index.vue";
+import Home from "./components/home/index.vue";
 
 export default {
   name: "App",
 
   components: {
-    Login,
+    Home,
   },
 
   data: () => ({
--- proyecto-final/src/components/home/home.js
+++ proyecto-final/src/components/home/home.js
@@ -1,22 +1,34 @@
+import products from "../../data/products.json";
+
+console.log(products);
 
 export default {
-  name: 'home',
+  name: "home",
   components: {},
   props: [],
-  data () {
-    return {
+  data: () => ({
+    cards: [
+      {
+        title: "Pre-fab homes",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 6,
+      },
+      {
+        title: "Favorite road trips",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 6,
+      },
+    ],
+    products: products,
+    flex: 6,
+  }),
 
-    }
-  },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-
-  }
-}
-
-
+  computed: {},
+  mounted() {},
+  methods: {},
+};
