321bd00820bed59f0e7877ce83a34f0c9bd17302
aarce <Arce.Alexis@hotmail.com>
2021-12-06 00:44:05 -0300

    Remove unused data from home component

--- proyecto-final/src/components/home/home.js
+++ proyecto-final/src/components/home/home.js
@@ -7,23 +7,6 @@ export default {
   components: {},
   props: [],
   data: () => ({
-    cards: [
-      {
-        title: "Pre-fab homes",
-        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-        flex: 6,
-      },
-      {
-        title: "Favorite road trips",
-        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-        flex: 6,
-      },
-      {
-        title: "Best airlines",
-        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
-        flex: 6,
-      },
-    ],
     products: products,
     flex: 6,
   }),
